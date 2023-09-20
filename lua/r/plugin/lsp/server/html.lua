f0d36b075f4647ef3effe44068c46e4c2460b5e8
rrrrafael <rafael.rodrigues@tuta.io>
2023-09-20 13:23:30 +0200

    Fix hmtl capabilities

--- lua/r/plugin/lsp/server/html.lua
+++ lua/r/plugin/lsp/server/html.lua
@@ -1,8 +1,5 @@
-local capabilities = function()
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-  return capabilities
-end
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 return {
   {
